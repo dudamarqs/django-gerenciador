156b023aa4f5e9f889dea7ceac0be012b7a33746
Maria Eduarda Marques <eduardamarquesnoleto@gmail.com>
2025-05-22 14:55:07 -0300

    crud de produtos

--- produtos/models.py
+++ produtos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=120)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
     estoque = models.IntegerField()
 
